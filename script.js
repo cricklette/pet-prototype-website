9d5a10c80c900e100ff914a674601f37720e9a50
cricklette <ellahlincoln@gmail.com>
2025-02-11 16:56:05 -0800

    update keyterms

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         cats: ["cats", "cat", "kittens", "kitten", "feline"],
         dogs: ["dogs", "dog", "puppies", "activity"],
         birds: ["birds", "bird", "parrot", "parrots"],
-        contact: ["contact", "contact us", "contact information", "schedule", "appointment"],
+        contact: ["contact", "contact us", "contact information", "schedule", "appointment", "hours", "location", "help", "help center", "helpdesk", "support", "support center", "supportdesk", "contact support", "contact help", "contact help center", "contact support center", "contact supportdesk", "contact helpdesk", "contact contact", "contact contact us", "contact hours", "contact location", "contact appointment", "contact schedule"],
     };
 
     var searchButton = document.getElementById('searchbtn');
